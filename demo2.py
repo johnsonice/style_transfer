8de5acfb089aad331ed6238b659f575bf3937b28
Chengyu Huang <chengyuhuang16@gmail.com>
2017-08-26 16:10:15 -0400

    update demo2 file

--- demo2.py
+++ demo2.py
@@ -5,19 +5,19 @@ Created on Sat Aug 26 15:46:58 2017
 @author: huang
 """
 
-from neuro_style_tf import style_net_single as sns
+from style_net_single import neural_style as sns
 #%%
 options= {
         'max_iterations':20,
         'device':'/cpu:0',
-        'model_weights':'./neuro_style_tf/imagenet-vgg-verydeep-19.mat',
+        'model_weights':'./style_net_single/imagenet-vgg-verydeep-19.mat',
         'style_imgs_weights':[1.0],
-        'style_imgs_dir':'./neuro_style_tf/styles',
+        'style_imgs_dir':'./style_net_single/styles',
         'style_imgs':['starry-night.jpg'],
-        'content_img_dir':'./neuro_style_tf/image_input',
+        'content_img_dir':'./style_net_single/image_input',
         'content_img':'face.jpg',
         'init_img_type':'content',          ## default  ['random', 'content', 'style']
-        'img_output_dir':'./neuro_style_tf/image_output',
+        'img_output_dir':'./style_net_single/image_output',
         'img_name':'testing.jpg',
         'print_iterations': 5               ## print every 5 iteration for test 
           }
